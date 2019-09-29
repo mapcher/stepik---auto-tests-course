d66b0698dc9770adbf3a6127a8ce2647ed919602
nemoj <1c.shakhty@mail.ru>
2019-09-30 01:45:33 +0300

    committing a file

---
+++ lesson2.4_step8.py
@@ -0,0 +1,40 @@
+﻿from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button = browser.find_element_by_css_selector("button.btn")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+
+    button1 = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+    )
+    button.click()
+
+    element = browser.find_element_by_id('input_value')
+    result=calc(element.text)
+
+    element = browser.find_element_by_id('answer')
+    element.send_keys(result)
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(60)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
